afa341e1dc07c9f9f696bb75fae082da32ed786c
giulia.trentarossi <giulia.trentarossi@C11-NO93CVNX5M7>
2026-07-16 12:47:53 +0200

    html, css, js files per la parte del footer

--- Ricettario.js
+++ Ricettario.js
@@ -1,1 +1,13 @@
-// JavaScript source code
+const messaggio = document.querySelector("#messaggio");
+const form = document.querySelector("#newsletterForm");
+
+form.addEventListener("submit", function (event) {
+    // blocca il reload della pagina
+    event.preventDefault();
+
+    // nasconde il form
+    form.style.display = "none";
+
+    // mostra il messaggio
+    messaggio.classList.remove("nascosto");
+});
